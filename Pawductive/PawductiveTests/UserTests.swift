0f5bbed067362ae5c2af0cb5ede7a5892a1b5bbe
kokjz <kokjz727@gmail.com>
2026-06-11 16:20:51 +0800

    Fix: Update unit tests under UserTests to accommodate removal of enums FoodCatalog and ToyCatalog

--- Pawductive/PawductiveTests/UserTests.swift
+++ Pawductive/PawductiveTests/UserTests.swift
@@ -23,27 +23,27 @@ struct UserTests {
         // Spend 30 coins
         user.buy(food: .chickenDrumstick)
         #expect(user.coins == 20)
-        #expect(user.foodInventory[.chickenDrumstick] == 1)
+        #expect(user.foodInventory[Food.chickenDrumstick.name] == 1)
         
         // Insufficent coins
         user.buy(food: .chickenDrumstick) // Cost 30 coins
         #expect(user.coins == 20)
-        #expect(user.foodInventory[.chickenDrumstick] == 1)
+        #expect(user.foodInventory[Food.chickenDrumstick.name] == 1)
         
         // Spend 10 coins
         user.buy(food: .pumpkin)
         #expect(user.coins == 10)
-        #expect(user.foodInventory[.pumpkin] == 1)
+        #expect(user.foodInventory[Food.pumpkin.name] == 1)
         
         // Spend 10 coins
         user.buy(food: .pumpkin)
         #expect(user.coins == 0)
-        #expect(user.foodInventory[.pumpkin] == 2)
+        #expect(user.foodInventory[Food.pumpkin.name] == 2)
         
         // Insufficient coins
         user.buy(food: .corn) // Cost 5 coins
         #expect(user.coins == 0)
-        #expect(user.foodInventory[.corn] == nil)
+        #expect(user.foodInventory[Food.corn.name] == nil)
     }
     
     @Test @MainActor func testBuyToys() async throws {
@@ -52,54 +52,54 @@ struct UserTests {
         // Spend 80 coins
         user.buy(toy: .rubberDuck)
         #expect(user.coins == 20)
-        #expect(user.toyInventory[.rubberDuck] == 1)
+        #expect(user.toyInventory[Toy.rubberDuck.name] == 1)
         
         // Insufficient Coins
         user.buy(toy: .frisbee) // Cost 30 coins
         #expect(user.coins == 20)
-        #expect(user.toyInventory[.frisbee] == nil)
+        #expect(user.toyInventory[Toy.frisbee.name] == nil)
         
         // Spend 10 coins
         user.buy(toy: .tennisBall)
         #expect(user.coins == 10)
-        #expect(user.toyInventory[.tennisBall] == 1)
+        #expect(user.toyInventory[Toy.tennisBall.name] == 1)
         
         // Spend 10 coins
         user.buy(toy: .tennisBall)
         #expect(user.coins == 0)
-        #expect(user.toyInventory[.tennisBall] == 2)
+        #expect(user.toyInventory[Toy.tennisBall.name] == 2)
         
         // Insufficient coins
         user.buy(toy: .treeBranch) // Cost 5 coins
         #expect(user.coins == 0)
-        #expect(user.toyInventory[.treeBranch] == nil)
+        #expect(user.toyInventory[Toy.treeBranch.name] == nil)
     }
 
     @Test @MainActor func testGiveFood() async throws {
         let user = UserProfile()
-        user.foodInventory[.corn] = 2
+        user.foodInventory[Food.corn.name] = 2
         
         user.give(food: .corn)
-        #expect(user.foodInventory[.corn] == 1)
+        #expect(user.foodInventory[Food.corn.name] == 1)
         
         user.give(food: .corn)
-        #expect(user.foodInventory[.corn] == nil)
+        #expect(user.foodInventory[Food.corn.name] == nil)
         
         user.give(food: .corn)
-        #expect(user.foodInventory[.corn] == nil)
+        #expect(user.foodInventory[Food.corn.name] == nil)
     }
     
     @Test @MainActor func testGiveToy() async throws {
         let user = UserProfile()
-        user.toyInventory[.frisbee] = 2
+        user.toyInventory[Toy.frisbee.name] = 2
         
         user.give(toy: .frisbee)
-        #expect(user.toyInventory[.frisbee] == 1)
+        #expect(user.toyInventory[Toy.frisbee.name] == 1)
         
         user.give(toy: .frisbee)
-        #expect(user.toyInventory[.frisbee] == nil)
+        #expect(user.toyInventory[Toy.frisbee.name] == nil)
         
         user.give(toy: .frisbee)
-        #expect(user.toyInventory[.frisbee] == nil)
+        #expect(user.toyInventory[Toy.frisbee.name] == nil)
     }
 }
