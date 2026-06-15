1e593e87235c4159e1b1e235c66c151740b68c8f
kokjz <kokjz727@gmail.com>
2026-06-15 18:22:23 +0800

    feat: create new ProfileView

---
+++ Pawductive/Pawductive/User/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Pawductive
+//
+//  Created by Kok Jun Zhe on 15/6/26.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
