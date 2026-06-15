caac658b2c6da9827fab3abd9f204dc24b249989
kokjz <kokjz727@gmail.com>
2026-06-15 16:03:20 +0800

    feat: update DataContainer

--- Pawductive/Pawductive/DataContainer.swift
+++ Pawductive/Pawductive/DataContainer.swift
@@ -16,7 +16,7 @@ class DataContainer {
     
     // Initializes a model container with a user and a pet
     init(user: UserProfile = UserProfile(), pet: Pet = Pet(), loadInventory: Bool = true, inMemory: Bool = true) {
-        let schema = Schema([TaskItem.self, UserProfile.self, Pet.self, Modifier.self])
+        let schema = Schema([TaskItem.self, UserProfile.self, Pet.self, Modifier.self, UserStats.self])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: inMemory)
         do {
             modelContainer = try ModelContainer(for: schema, configurations: [modelConfiguration])
