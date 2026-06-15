e9fe17e86aaf5496459b426a944b7491ed3529fa
kokjz <kokjz727@gmail.com>
2026-06-15 18:17:04 +0800

    feat: complete skeletal Achievement

--- Pawductive/Pawductive/User/Achievement.swift
+++ Pawductive/Pawductive/User/Achievement.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+enum Achievement: String, CaseIterable, Identifiable {
+    //achievements
+    case testTaskAchievement
+    case testTimeAchievement
+    
+    var id: String { self.rawValue }
+    
+    //achievement titles
+    var title: String {
+        switch self {
+        case.testTaskAchievement: return "Test Task Achievement"
+        case.testTimeAchievement: return "Test Time Achievement"
+        }
+    }
+    
+    //unlock descriptions
+    var requirementDescription: String {
+        switch self {
+        case.testTaskAchievement: return "Test Task Achievement Description"
+        case.testTimeAchievement: return "Test Time Achievement Description"
+        }
+    }
+    
+    //unlock eval logic
+    func isunlocked(stats: UserStats) -> Bool {
+        switch self {
+        case.testTaskAchievement: return stats.totalTasksCompleted >= 1
+        case.testTimeAchievement: return stats.totalMinutesFocused >= 1
+        }
+    }
+}
