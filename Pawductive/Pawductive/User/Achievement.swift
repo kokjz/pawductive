4736a9e033918ddee0b78657673de752a9cd9da3
kokjz <kokjz727@gmail.com>
2026-06-15 22:01:41 +0800

    feat: update achievement list in Achievement

--- Pawductive/Pawductive/User/Achievement.swift
+++ Pawductive/Pawductive/User/Achievement.swift
@@ -9,32 +9,48 @@ import Foundation
 
 enum Achievement: String, CaseIterable, Identifiable {
     //achievements
-    case testTaskAchievement
-    case testTimeAchievement
+    case OneTaskAchievement
+    case FiveTasksAchievement
+    case TenTasksAchievement
+    case OneMinAchievement
+    case ThirtyMinAchievement
+    case OneHourAchievement
     
     var id: String { self.rawValue }
     
     //achievement titles
     var title: String {
         switch self {
-        case.testTaskAchievement: return "Test Task Achievement"
-        case.testTimeAchievement: return "Test Time Achievement"
+        case.OneTaskAchievement: return "New Worker"
+        case.FiveTasksAchievement: return "Nine to Five"
+        case.TenTasksAchievement: return "Workaholic"
+        case.OneMinAchievement: return "Clocking In"
+        case.ThirtyMinAchievement: return "Half Hour Hero"
+        case.OneHourAchievement: return "Overtime"
         }
     }
     
     //unlock descriptions
     var requirementDescription: String {
         switch self {
-        case.testTaskAchievement: return "Test Task Achievement Description"
-        case.testTimeAchievement: return "Test Time Achievement Description"
+        case.OneTaskAchievement: return "Complete 1 task"
+        case.FiveTasksAchievement: return "Complete 5 tasks"
+        case.TenTasksAchievement: return "Complete 10 tasks"
+        case.OneMinAchievement: return "Focus for 1 minute"
+        case.ThirtyMinAchievement: return "Focus for 30 minutes"
+        case.OneHourAchievement: return "Focus for 1 hour"
         }
     }
     
     //unlock eval logic
     func isunlocked(stats: UserStats) -> Bool {
         switch self {
-        case.testTaskAchievement: return stats.totalTasksCompleted >= 1
-        case.testTimeAchievement: return stats.totalMinutesFocused >= 1
+        case.OneTaskAchievement: return stats.totalTasksCompleted >= 1
+        case.FiveTasksAchievement: return stats.totalTasksCompleted >= 5
+        case.TenTasksAchievement: return stats.totalTasksCompleted >= 10
+        case.OneMinAchievement: return stats.totalMinutesFocused >= 1
+        case.ThirtyMinAchievement: return stats.totalMinutesFocused >= 30
+        case.OneHourAchievement: return stats.totalMinutesFocused >= 60
         }
     }
 }
