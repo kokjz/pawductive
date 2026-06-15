149bfe5f937ee2e8cc70a0ceb9eba065f86d6a43
kokjz <kokjz727@gmail.com>
2026-06-15 15:56:30 +0800

    feat: complete UserStats

--- Pawductive/Pawductive/User/UserStats.swift
+++ Pawductive/Pawductive/User/UserStats.swift
@@ -8,4 +8,32 @@
 import Foundation
 import SwiftData
 
-
+@Model
+final class UserStats {
+    @Attribute(.unique) var id: UUID
+    
+    //cumulative stats
+    var totalTasksCompleted: Int
+    var totalMinutesFocused: Int
+    var totalCoinsEarned: Int
+    
+    //streak tracking
+    var currentStreak: Int
+    var lastActiveDate: Date?
+    
+    init(
+        id: UUID = UUID(),
+        totalTasksCompleted: Int = 0,
+        totalMinutesFocused: Int = 0,
+        totalCoinsEarned: Int = 0,
+        currentStreak: Int = 0,
+        lastActiveDate: Date? = nil
+    ) {
+        self.id = UUID()
+        self.totalTasksCompleted = totalTasksCompleted
+        self.totalMinutesFocused = totalMinutesFocused
+        self.totalCoinsEarned = totalCoinsEarned
+        self.currentStreak = currentStreak
+        self.lastActiveDate = lastActiveDate
+    }
+}
