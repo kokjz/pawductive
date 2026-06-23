2e47ed128e407a86fddaf43e34d6e2f24b6a641b
kokjz <kokjz727@gmail.com>
2026-06-23 16:10:45 +0800

    feat: create new SettingsManager and SettingsManagerView

---
+++ Pawductive/Pawductive/Settings/SettingsManager.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsManager.swift
+//  Pawductive
+//
+//  Created by Kok Jun Zhe on 23/6/26.
+//
+
+import Foundation
---
+++ Pawductive/Pawductive/Settings/SettingsManagerView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsManagerView.swift
+//  Pawductive
+//
+//  Created by Kok Jun Zhe on 23/6/26.
+//
+
+import SwiftUI
+
+struct SettingsManagerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsManagerView()
+}
