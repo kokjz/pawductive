9ecdad17ed4c7dd134a7c1730fc9b5821520df7d
kokjz <kokjz727@gmail.com>
2026-06-23 16:32:01 +0800

    feat: complete SettingsManager

--- Pawductive/Pawductive/Settings/SettingsManager.swift
+++ Pawductive/Pawductive/Settings/SettingsManager.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import Observation
+
+@Observable
+class SettingsManager {
+    static let shared = SettingsManager()
+    
+    var isTimerPauseEnabled: Bool {
+        didSet { UserDefaults.standard.set(isTimerPauseEnabled, forKey: "isTimerPauseEnabled") }
+    }
+    
+    private init() {
+        //set to false by default
+        self.isTimerPauseEnabled = UserDefaults.standard.object(forKey: "isTimerPauseEnabled") as? Bool ?? false
+    }
+}
