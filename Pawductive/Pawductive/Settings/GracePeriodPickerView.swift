ffb3b95363b3afe76baca7f22f5968a1a2658c25
kokjz <kokjz727@gmail.com>
2026-06-24 14:39:46 +0800

    feat: create new GracePeriodPickerView

---
+++ Pawductive/Pawductive/Settings/GracePeriodPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  GracePeriodPickerView.swift
+//  Pawductive
+//
+//  Created by Kok Jun Zhe on 24/6/26.
+//
+
+import SwiftUI
+
+struct GracePeriodPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GracePeriodPickerView()
+}
