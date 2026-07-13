a83c77564c2b1e3f75d4483598b044ed944caa1c
zirong <zirong679@gmail.com>
2026-07-13 17:01:49 +0800

    Added pet statistics and achievements to profile view. Added test for pet statistics.

--- Pawductive/Pawductive/User/Achievement.swift
+++ Pawductive/Pawductive/User/Achievement.swift
@@ -15,6 +15,18 @@ enum Achievement: String, CaseIterable, Identifiable {
     case OneMinAchievement
     case ThirtyMinAchievement
     case OneHourAchievement
+    case GiveFoodAchievement_3
+    case GiveFoodAchievement_30
+    case GiveFoodAchievement_300
+    case GiveToysAchievement_3
+    case GiveToysAchievement_30
+    case GiveToysAchievement_300
+    case MoodStreakAchievement_3
+    case MoodStreakAchievement_30
+    case MoodStreakAchievement_300
+    case EnergyStreakAchievement_3
+    case EnergyStreakAchievement_30
+    case EnergyStreakAchievement_300
     
     var id: String { self.rawValue }
     
@@ -27,6 +39,18 @@ enum Achievement: String, CaseIterable, Identifiable {
         case.OneMinAchievement: return "Clocking In"
         case.ThirtyMinAchievement: return "Half Hour Hero"
         case.OneHourAchievement: return "Overtime"
+        case.GiveFoodAchievement_3: return "First Bite"
+        case.GiveFoodAchievement_30: return "Regular Meals"
+        case.GiveFoodAchievement_300: return "Royal Banquet"
+        case.GiveToysAchievement_3: return "Toy Box"
+        case.GiveToysAchievement_30: return "Toy Enthusiast"
+        case.GiveToysAchievement_300: return "Toy Collector"
+        case.MoodStreakAchievement_3: return "Bright Days"
+        case.MoodStreakAchievement_30: return "Bundle of Joy"
+        case.MoodStreakAchievement_300: return "Ray of Sunshine"
+        case.EnergyStreakAchievement_3: return "Energized"
+        case.EnergyStreakAchievement_30: return "Wide Awake"
+        case.EnergyStreakAchievement_300: return "Never Tired"
         }
     }
     
@@ -39,11 +63,23 @@ enum Achievement: String, CaseIterable, Identifiable {
         case.OneMinAchievement: return "Focus for 1 minute"
         case.ThirtyMinAchievement: return "Focus for 30 minutes"
         case.OneHourAchievement: return "Focus for 1 hour"
+        case.GiveFoodAchievement_3: return "Give 3 food"
+        case.GiveFoodAchievement_30: return "Give 30 food"
+        case.GiveFoodAchievement_300: return "Give 300 food"
+        case.GiveToysAchievement_3: return "Give 3 toys"
+        case.GiveToysAchievement_30: return "Give 30 toys"
+        case.GiveToysAchievement_300: return "Give 300 toys"
+        case.MoodStreakAchievement_3: return "Keep mood high for 3 days"
+        case.MoodStreakAchievement_30: return "Keep mood high for 30 days"
+        case.MoodStreakAchievement_300: return "Keep mood high for 300 days"
+        case.EnergyStreakAchievement_3: return "Keep energy high for 3 days"
+        case.EnergyStreakAchievement_30: return "Keep energy high for 30 days"
+        case.EnergyStreakAchievement_300: return "Keep energy high for 300 days"
         }
     }
     
     //unlock eval logic
-    func isUnlocked(stats: UserStats) -> Bool {
+    func isUnlocked(stats: UserStats, pet: Pet) -> Bool {
         switch self {
         case.OneTaskAchievement: return stats.totalTasksCompleted >= 1
         case.FiveTasksAchievement: return stats.totalTasksCompleted >= 5
@@ -51,6 +87,18 @@ enum Achievement: String, CaseIterable, Identifiable {
         case.OneMinAchievement: return stats.totalMinutesFocused >= 1
         case.ThirtyMinAchievement: return stats.totalMinutesFocused >= 30
         case.OneHourAchievement: return stats.totalMinutesFocused >= 60
+        case.GiveFoodAchievement_3: return pet.totalFoodReceived >= 3
+        case.GiveFoodAchievement_30: return pet.totalFoodReceived >= 30
+        case.GiveFoodAchievement_300: return pet.totalFoodReceived >= 300
+        case.GiveToysAchievement_3: return pet.totalToysReceived >= 3
+        case.GiveToysAchievement_30: return pet.totalToysReceived >= 30
+        case.GiveToysAchievement_300: return pet.totalToysReceived >= 300
+        case.MoodStreakAchievement_3: return pet.maxHighMoodStreak >= 3
+        case.MoodStreakAchievement_30: return pet.maxHighMoodStreak >= 30
+        case.MoodStreakAchievement_300: return pet.maxHighMoodStreak >= 300
+        case.EnergyStreakAchievement_3: return pet.maxHighEnergyStreak >= 3
+        case.EnergyStreakAchievement_30: return pet.maxHighEnergyStreak >= 30
+        case.EnergyStreakAchievement_300: return pet.maxHighEnergyStreak >= 300
         }
     }
 }
