92398348730ddc156aa090cecb7303ad3be84c17
kokjz <kokjz727@gmail.com>
2026-07-21 16:49:17 +0800

    feat: new TaskCategory

---
+++ Pawductive/Pawductive/Tasks/TaskCategory.swift
@@ -0,0 +1,30 @@
+//
+//  TaskCategory.swift
+//  Pawductive
+//
+//  Created by Kok Jun Zhe on 21/7/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class TaskCategory {
+    @Attribute(.unique) var id: UUID
+    var name: String
+    var iconName: String
+    
+    init(name: String, iconName: String = "folder.fill") {
+        self.id = UUID()
+        self.name = name
+        self.iconName = iconName
+    }
+    
+    static let defaults: [TaskCategory] = [
+        TaskCategory(name: "Study", iconName: "book.fill"),
+        TaskCategory(name: "Work", iconName: "briefcase.fill"),
+        TaskCategory(name: "Fitness", iconName: "figure.run"),
+        TaskCategory(name: "Leisure", iconName: "gamecontroller.fill"),
+        TaskCategory(name: "General", iconName: "folder.fill")
+    ]
+}
