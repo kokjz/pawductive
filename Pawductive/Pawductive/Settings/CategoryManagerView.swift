59d60ac641d2b757a8ab70f7635a826bd352d15f
kokjz <kokjz727@gmail.com>
2026-07-22 23:57:37 +0800

    feat: create new stub CategoryManagerView

---
+++ Pawductive/Pawductive/Settings/CategoryManagerView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryManagerView.swift
+//  Pawductive
+//
+//  Created by Kok Jun Zhe on 22/7/26.
+//
+
+import SwiftUI
+
+struct CategoryManagerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryManagerView()
+}
