a375786fd3e733f0c4d8f8e19a193e9179b79b5e
kokjz <kokjz727@gmail.com>
2026-07-22 18:45:25 +0800

    feat: create new TaskCreationView

---
+++ Pawductive/Pawductive/Tasks/TaskCreationView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskCreationView.swift
+//  Pawductive
+//
+//  Created by Kok Jun Zhe on 22/7/26.
+//
+
+import SwiftUI
+
+struct TaskCreationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskCreationView()
+}
