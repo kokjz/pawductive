5d64e2f2a6f364b566da6f96c38348617c03801d
kokjz <kokjz727@gmail.com>
2026-07-24 02:24:55 +0800

    feat: add new StatsDashboardView

---
+++ Pawductive/Pawductive/Analytics/StatsDashboardView.swift
@@ -0,0 +1,18 @@
+//
+//  StatsDashboardView.swift
+//  Pawductive
+//
+//  Created by Kok Jun Zhe on 24/7/26.
+//
+
+import SwiftUI
+
+struct StatsDashboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatsDashboardView()
+}
