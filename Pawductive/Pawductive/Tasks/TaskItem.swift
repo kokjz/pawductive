ce90087670f0f5c78fa0a01daf36ebb65b8b6f46
kokjz <kokjz727@gmail.com>
2026-07-23 21:16:27 +0800

    feat: add sortOrder property to TaskItem

--- Pawductive/Pawductive/Tasks/TaskItem.swift
+++ Pawductive/Pawductive/Tasks/TaskItem.swift
@@ -16,13 +16,15 @@ final class TaskItem {
     var isCompleted: Bool
     var creationDate: Date
     var categoryName: String
+    var sortOrder: Int
     
-    init(title: String, expectedDurationInMinutes: Int, categoryName: String = "General") {
+    init(title: String, expectedDurationInMinutes: Int, categoryName: String = "General", sortOrder: Int = 0) {
         self.id = UUID()
         self.title = title
         self.expectedDurationInMinutes = expectedDurationInMinutes
         self.isCompleted = false
         self.creationDate = Date()
         self.categoryName = categoryName
+        self.sortOrder = sortOrder
     }
 }
