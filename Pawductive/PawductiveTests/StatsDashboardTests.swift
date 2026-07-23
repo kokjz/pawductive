b5ef8d909d5c22c14597209776c95319484210f3
kokjz <kokjz727@gmail.com>
2026-07-24 04:21:51 +0800

    test: create new StatsDashboardTests

---
+++ Pawductive/PawductiveTests/StatsDashboardTests.swift
@@ -0,0 +1,10 @@
+//
+//  StatsDashboardTests.swift
+//  PawductiveTests
+//
+//  Created by Kok Jun Zhe on 24/7/26.
+//
+
+import Foundation
+
+//im too fuckin tired ill deal with this tomorrow gn chat
