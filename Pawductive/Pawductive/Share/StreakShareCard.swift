0a31bfef8e0eb4a7533f85674e46203bfd9a811f
kokjz <kokjz727@gmail.com>
2026-07-24 14:44:20 +0800

    feat: create new StreakShareCard under new subdir Share

---
+++ Pawductive/Pawductive/Share/StreakShareCard.swift
@@ -0,0 +1,18 @@
+//
+//  StreakShareCard.swift
+//  Pawductive
+//
+//  Created by Kok Jun Zhe on 24/7/26.
+//
+
+import SwiftUI
+
+struct StreakShareCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StreakShareCard()
+}
